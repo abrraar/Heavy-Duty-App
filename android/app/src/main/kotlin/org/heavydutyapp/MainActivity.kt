d31ca128058bb37dbb24f37e091556a1c28d1090
abrraar <md.alabrar.mjd@gmail.com>
2026-08-20 04:58:56 +0530

    Revert: Restored default Android 12+ splash screen behavior and removed custom launch branding

--- android/app/src/main/kotlin/org/heavydutyapp/MainActivity.kt
+++ android/app/src/main/kotlin/org/heavydutyapp/MainActivity.kt
@@ -2,25 +2,14 @@ package org.heavydutyapp
 
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.WindowManager
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        Log.d("SplashDebug", "MainActivity onCreate started")
-
         // Install the splash screen via the compat library
-        val splashScreen = installSplashScreen()
-
-        // Handle Android 12+ Splash Screen Exit instantly
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            splashScreen.setOnExitAnimationListener { splashScreenView ->
-                Log.d("SplashDebug", "Exit Animation Listener TRIGGERED - Removing splash immediately")
-                splashScreenView.remove()
-            }
-        }
+        installSplashScreen()
 
         super.onCreate(savedInstanceState)
         
--- lib/features/auth/provider/auth_provider.dart
+++ lib/features/auth/provider/auth_provider.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import '../../profile/model/user_email.dart';
