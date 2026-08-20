d7c00fb775a87fe1e50d1c6e3e7ccca3f49cfbbf
abrraar <md.alabrar.mjd@gmail.com>
2026-08-20 05:49:55 +0530

    Revert system splash screen to default behavior

--- android/app/src/main/kotlin/org/heavydutyapp/MainActivity.kt
+++ android/app/src/main/kotlin/org/heavydutyapp/MainActivity.kt
@@ -3,14 +3,10 @@ package org.heavydutyapp
 import android.os.Build
 import android.os.Bundle
 import android.view.WindowManager
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import io.flutter.embedding.android.FlutterActivity
 
 class MainActivity: FlutterActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
-        // Install the splash screen via the compat library
-        installSplashScreen()
-
         super.onCreate(savedInstanceState)
         
         // Allow activity to show over lock screen and turn screen on
